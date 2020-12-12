b73f8ed8495eaf366f41ceb80caf9963da4621ad
BulbaWarrior <vladkalm49@gmail.com>
2020-12-12 20:57:28 +0300

    output ip's using splat expression

--- output.tf
+++ output.tf
@@ -1,5 +1,5 @@
 output "web_host_public_ips" {
-  value = [aws_instance.webapp_host[0].public_ip, aws_instance.webapp_host[1].public_ip]
+  value = aws_instance.webapp_host[*].public_ip
 }
 
 output "db_public_ip" {

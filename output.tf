1ae57b5f2276ffb448967afd856375dd3aa67b99
BulbaWarrior <vladkalm49@gmail.com>
2020-12-12 17:31:07 +0300

    fix outputs

--- output.tf
+++ output.tf
@@ -1,6 +1,5 @@
 output "web_host_public_ips" {
-  for_each = aws_instance.webapp_host
-  value = each.public_ip
+  value = [aws_instance.webapp_host[0].public_ip, aws_instance.webapp_host[1].public_ip]
 }
 
 output "db_public_ip" {

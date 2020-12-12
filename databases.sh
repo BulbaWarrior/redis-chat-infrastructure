a9d354481e18293419b4e63f828e504ad46a96aa
Sergey Semushin <s.semushin@innopolis.university>
2020-12-12 18:24:35 +0300

    redis now running in append only mode

--- databases.sh
+++ databases.sh
@@ -2,7 +2,7 @@
 
 sudo curl -sLS get.docker.com | sh
 
-sudo docker run -p 6379:6379 --name redis -d redis
-# sudo docker run -p 6379:6379 --name redis -d redis redis-server --appendonly yes
+# sudo docker run -p 6379:6379 --name redis -d redis
+sudo docker run -p 6379:6379 --name redis -d redis redis-server --appendonly yes
 
 sudo docker run -p 5432:5432 --name postgres -d --env POSTGRES_DB=postgres --env POSTGRES_USER=postgres --env POSTGRES_PASSWORD=postgres postgres

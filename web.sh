fee1f06cd0de4e5a4784aedca9486ac9182d16e2
Sergey Semushin <s.semushin@innopolis.university>
2020-12-12 19:37:29 +0300

    Now webserver uses docker

--- web.sh
+++ web.sh
@@ -1,24 +1,5 @@
 #!/bin/bash
 
-sudo apt update
-
-# download repo from git
-sudo apt install git -y
-cd /
-sudo git clone https://github.com/kezzyhko/django-redis-chat.git
-cd /django-redis-chat
-
-# install python
-sudo apt install python3 -y
-sudo apt install python3-pip -y
-sudo pip3 install -r requirements.txt
-
-# configure ip addresses
-echo "${database_addr} redis" | sudo tee -a /etc/hosts
-echo "${database_addr} postgres" | sudo tee -a /etc/hosts
-	
-# start django
-sudo python3 manage.py collectstatic --no-input
-sudo python3 manage.py makemigrations
-sudo python3 manage.py migrate --run-syncdb
-sudo python3 -u manage.py runserver 0.0.0.0:8000 --noreload
+sudo curl -sLS get.docker.com | sh
+sudo docker build https://github.com/kezzyhko/django-redis-chat.git#main --tag django-redis-chat
+sudo docker run -p 80:8000 --name django-redis-chat -d --env POSTGRES=${database_addr} --env REDIS=${database_addr} django-redis-chat bash -c "python3 manage.py makemigrations && python3 manage.py migrate --run-syncdb && python3 -u manage.py runserver 0.0.0.0:8000 --noreload"

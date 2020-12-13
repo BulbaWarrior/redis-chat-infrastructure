f5b2e8a2a454ecf6a9301d5f1ba442ad5b0fe0ec
BulbaWarrior <vladkalm49@gmail.com>
2020-12-13 15:52:10 +0300

    fix ilya's typos

--- instances.tf
+++ instances.tf
@@ -43,13 +43,13 @@ resource "aws_instance" "loadbalancer_host" {
 }
 
 resource "aws_instance" "backup_host" {
-  ami                         = var.amisp[var.region]
+  ami                         = var.amis[var.region]
   instance_type               = "t2.micro"
   key_name                    = var.keys[var.region]
   subnet_id                   = aws_subnet.local_network.id
   associate_public_ip_address = true # for debug purposes
   vpc_security_group_ids      = [aws_security_group.local_sg.id]
-  user_data                   = templatefile("backup.sh", { database_addr = var.database_addr })
+  user_data                   = templatefile("backup.sh", { database_addr = local.database_addr })
   tags = {
     Name = "backup server"
   }

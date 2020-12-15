40e33710db986e02d8c5957a086b34ca2269217e
BulbaWarrior <vladkalm49@gmail.com>
2020-12-15 15:40:33 +0300

    fix cicd configuration

--- cicd.sh
+++ cicd.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 sudo curl -sLS get.docker.com | sh
-sudo mkdir -p /jenkinshome
+sudo mkdir -p /jenkins_home
+sudo chown 1000 /jenkins_home
 sudo docker run -p 80:8080 -p 50000:50000 -v /jenkins_home:/var/jenkins_home jenkins/jenkins:lts

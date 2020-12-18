0801c6e0d7776bd6b1d804ef0906e543d8f195de
Sergey Semushin <s.semushin@innopolis.university>
2020-12-18 17:30:25 +0300

    Added debconf selection: bacula-director-pgsql/remote/host localhost

--- backup.sh
+++ backup.sh
@@ -17,6 +17,7 @@ sudo debconf-set-selections <<< 'postfix postfix/main_mailer_type select Local o
 sudo debconf-set-selections <<< 'postfix postfix/mailname string backup'
 sudo debconf-set-selections <<< 'bacula-director-sqlite3 bacula-director-sqlite3/dbconfig-install boolean true'
 sudo debconf-set-selections <<< 'bacula-director-pgsql bacula-director-pgsql/dbconfig-install boolean true'
+sudo debconf-set-selections <<< 'bacula-director-pgsql bacula-director-pgsql/remote/host select localhost'
 #sudo debconf-set-selections <<< 'dbconfig-common dbconfig-common/pgsql/admin-pass password ${bacula_database_pass}'
 #sudo debconf-set-selections <<< 'bacula-director-sqlite3 bacula-director-sqlite3/password-confirm password ${bacula_database_pass}'
 sudo apt install bacula-server -y

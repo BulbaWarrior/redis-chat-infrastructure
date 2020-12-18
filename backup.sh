9e73b775cd4d34c3a7ee0af08432bfdf47a7f8c1
Sergey Semushin <s.semushin@innopolis.university>
2020-12-18 17:06:25 +0300

    Added handling for bacula-director-pgsql

--- backup.sh
+++ backup.sh
@@ -16,6 +16,7 @@ sudo apt install mysql-server -y
 sudo debconf-set-selections <<< 'postfix postfix/main_mailer_type select Local only'
 sudo debconf-set-selections <<< 'postfix postfix/mailname string backup'
 sudo debconf-set-selections <<< 'bacula-director-sqlite3 bacula-director-sqlite3/dbconfig-install boolean true'
+sudo debconf-set-selections <<< 'bacula-director-pgsql bacula-director-pgsql/dbconfig-install boolean true'
 #sudo debconf-set-selections <<< 'dbconfig-common dbconfig-common/pgsql/admin-pass password ${bacula_database_pass}'
 #sudo debconf-set-selections <<< 'bacula-director-sqlite3 bacula-director-sqlite3/password-confirm password ${bacula_database_pass}'
 sudo apt install bacula-server -y

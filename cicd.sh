4bf8ca86033d2c6c97b5546c320e92bfb1b5ef28
BulbaWarrior <vladkalm49@gmail.com>
2020-12-19 14:43:10 +0300

    use jenkins with docker docker image

--- cicd.sh
+++ cicd.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 sudo curl -sLS get.docker.com | sh
-sudo mkdir -p /jenkins_home
+sudo apt install -y unzip
+sudo unzip /tmp/jenkins_home.zip -d /
 sudo chown 1000 /jenkins_home
-sudo docker run -p 80:8080 -p 50000:50000 -v /jenkins_home:/var/jenkins_home jenkins/jenkins:lts
+sudo docker run -p 80:8080 -v /jenkins_home:/var/jenkins_home -v /var/run/docker.sock:/var/run/docker.sock gustavoapolinario/jenkins-docker
+

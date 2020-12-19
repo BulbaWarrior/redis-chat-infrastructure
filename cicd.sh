cd48e08b167b15fc2357f286953271f63e20df03
BulbaWarrior <vladkalm49@gmail.com>
2020-12-19 16:57:11 +0300

    use fresh installation of jenkins for now

--- cicd.sh
+++ cicd.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 sudo curl -sLS get.docker.com | sh
-sudo apt install -y unzip
-sudo unzip /tmp/jenkins_home.zip -d /
+#sudo apt install -y unzip
+#sudo unzip /tmp/jenkins_home.zip -d /
+sudo mkdir /jenkins_home
 sudo chown 1000 /jenkins_home
 sudo docker run -p 80:8080 -v /jenkins_home:/var/jenkins_home -v /var/run/docker.sock:/var/run/docker.sock gustavoapolinario/jenkins-docker
 

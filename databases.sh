8be23366e27749c4b846381a9f461202de70ba76
Sergey Semushin <s.semushin@innopolis.university>
2020-12-19 14:53:57 +0300

    Now docker uses volumes to mount from inside container to host

--- databases.sh
+++ databases.sh
@@ -7,4 +7,5 @@ sudo docker run -p 6379:6379 --name redis -d redis redis-server --appendonly yes
 
 sudo mkdir -p /data
 sudo chown 1000 /data
-sudo docker run -p 5432:5432 --mount type=bind,source=/var/lib/postgresql/data,target=/data --name postgres -d --env POSTGRES_DB=postgres --env POSTGRES_USER=postgres --env POSTGRES_PASSWORD=postgres postgres
+sudo docker volume create --driver local --opt type=none --opt device=/data --opt o=bind data
+sudo docker run -p 5432:5432 -v data:/var/lib/postgresql/data --name postgres -d --env POSTGRES_DB=postgres --env POSTGRES_USER=postgres --env POSTGRES_PASSWORD=postgres postgres
--- web.sh
+++ web.sh
@@ -5,4 +5,5 @@ sudo chown 1000 /webapp
 
 sudo curl -sLS get.docker.com | sh
 sudo docker build https://github.com/kezzyhko/django-redis-chat.git#main --tag django-redis-chat
-sudo docker run -p 80:8000 --mount type=bind,source=/web,target=/webapp --name django-redis-chat -d --env POSTGRES=${database_addr} --env REDIS=${database_addr} --env NODE_ID=${node_id} django-redis-chat bash -c "python3 manage.py makemigrations && python3 manage.py migrate --run-syncdb && python3 -u manage.py runserver 0.0.0.0:8000 --noreload"
+sudo docker volume create --driver local --opt type=none --opt device=/webapp --opt o=bind webapp
+sudo docker run -p 80:8000 -v webapp:/web --name django-redis-chat -d --env POSTGRES=${database_addr} --env REDIS=${database_addr} --env NODE_ID=${node_id} django-redis-chat bash -c "python3 manage.py makemigrations && python3 manage.py migrate --run-syncdb && python3 -u manage.py runserver 0.0.0.0:8000 --noreload"
